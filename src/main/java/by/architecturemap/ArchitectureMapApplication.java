1a3e3dc380bfb3c2e54c252ead74dc53d0eb391b
Nadia Shokel <primisen17@gmail.com>
2022-07-18 17:35:07 +0300

    Initialization commit

---
+++ src/main/java/by/architecturemap/ArchitectureMapApplication.java
@@ -0,0 +1,13 @@
+package by.architecturemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/architecturemap/ArchitectureMapApplicationTests.java
@@ -0,0 +1,13 @@
+package by.architecturemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
