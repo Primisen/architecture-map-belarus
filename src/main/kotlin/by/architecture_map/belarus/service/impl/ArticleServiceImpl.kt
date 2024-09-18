d1fbaaf41a6040655795d47fb7603cfd4fda900b
Nadzeya <primisen17@gmail.com>
2024-09-18 13:52:44 +0200

    Realized ArticleService and ArticleController

--- src/main/kotlin/by/architecture_map/belarus/service/impl/ArticleServiceImpl.kt
+++ src/main/kotlin/by/architecture_map/belarus/service/impl/ArticleServiceImpl.kt
@@ -1,7 +1,51 @@
 package by.architecture_map.belarus.service.impl
 
+import by.architecture_map.belarus.entity.Article
+import by.architecture_map.belarus.exception.NotFoundException
+import by.architecture_map.belarus.repository.jpa.ArticleRepository
+import by.architecture_map.belarus.service.ArticleService
 import org.springframework.stereotype.Service
 
 @Service
-class ArticleServiceImpl {
+class ArticleServiceImpl(
+    private val articleRepository: ArticleRepository
+) : ArticleService {
+    override fun create(article: Article): Article = articleRepository.save(article)
+
+    override fun find(id: Int): Article = articleRepository.findById(id)
+        .orElseThrow { throw NotFoundException("Construction not found with id: $id") }
+
+    override fun findAll(): List<Article> = articleRepository.findAll()
+
+    override fun update(id: Int, updatedArticle: Article): Article =
+        applyUpdate(id) {
+            title = updatedArticle.title
+            content = updatedArticle.content
+            shortDescription = updatedArticle.shortDescription
+            demonstrativeImage = updatedArticle.demonstrativeImage
+            tags = updatedArticle.tags
+        }
+
+    override fun patchUpdate(id: Int, updatedArticle: Article): Article =
+        applyUpdate(id) {
+            if (!updatedArticle.title.isNullOrEmpty())
+                title = updatedArticle.title
+            if (!updatedArticle.content.isNullOrEmpty())
+                content = updatedArticle.content
+            if (!updatedArticle.shortDescription.isNullOrEmpty())
+                shortDescription = updatedArticle.shortDescription
+            if (updatedArticle.demonstrativeImage != null)
+                demonstrativeImage = updatedArticle.demonstrativeImage
+            if (!updatedArticle.tags.isNullOrEmpty())
+                tags = updatedArticle.tags
+        }
+
+    override fun delete(id: Int) {
+        find(id).also { articleRepository.deleteById(id) }
+    }
+
+    private fun applyUpdate(id: Int, update: Article.() -> Unit): Article =
+        find(id)
+            .apply(update)
+            .let { articleRepository.save(it) }
 }
