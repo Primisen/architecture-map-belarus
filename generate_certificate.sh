ed76bc176aa257d501b5973386fb048c3f116fe4
ilya.gayduk <ilya.gayduk@norstella.com>
2025-04-26 10:23:48 +0200

    update generate_certificate.sh script to work with different container runtimes

--- generate_certificate.sh
+++ generate_certificate.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-docker run --rm -v $(pwd)/certs:/certs -u root -v .:/usr/share/elasticsearch/config/certificates docker.elastic.co/elasticsearch/elasticsearch:7.17.24  bin/elasticsearch-certutil cert --in config/certificates/instances.yml --pem -out /certs/bundle.zip
-unzip certs/bundle.zip -d certs
-docker run --rm --entrypoint cat openjdk:17-jdk-alpine /etc/ssl/certs/java/cacerts > cacerts
-docker run --rm -v `pwd`:/tmp/certs openjdk:17-jdk-alpine sh -c 'cd /tmp/certs && keytool -keystore cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias elastic -file certs/es01/es01.crt'
+
+# set this to 'docker' or 'nerdctl' depending on your setup
+export container_exec="docker"
+
+$container_exec run --rm -v $(pwd)/certs:/certs -u root -v .:/usr/share/elasticsearch/config/certificates docker.elastic.co/elasticsearch/elasticsearch:7.17.24  bin/elasticsearch-certutil cert --in config/certificates/instances.yml --pem -out /certs/bundle.zip
+sudo unzip certs/bundle.zip -d certs
+$container_exec run --rm --entrypoint cat openjdk:17-jdk-alpine /etc/ssl/certs/java/cacerts > cacerts
+$container_exec run --rm -v `pwd`:/tmp/certs openjdk:17-jdk-alpine sh -c 'cd /tmp/certs && keytool -keystore cacerts -storepass changeit -noprompt -trustcacerts -importcert -alias elastic -file certs/es01/es01.crt'
